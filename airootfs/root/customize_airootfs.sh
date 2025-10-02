398cd46ecd15eeba5d8c477fa37da63d48268eb9
aislxflames <proarghaop@gmail.com>
2025-10-02 12:34:24 +0530

    AsiraOS fixed

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Add GPG keys during ISO build
+ 
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mkdir -p {out,work}
-
+curl -sSL https://asiraos.github.io/core/asiraos-core.pubkey.asc | sudo pacman-key --add -
 sudo mkarchiso -v -w work -o out .
