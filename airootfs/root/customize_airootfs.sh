9f73cff2d5614e6a1714d54a737756743b34c8ee
aislxflames <proarghaop@gmail.com>
2026-02-15 16:06:57 +0530

    Disable Asira repo key checks and key refresh steps

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -5,11 +5,4 @@ echo "[*] Initializing pacman keyring..."
 pacman-key --init
 pacman-key --populate archlinux
 
-echo "[*] Adding AsiraOS core repository key..."
-curl -sSL https://asiraos.github.io/core/asiraos-core.pubkey.asc | pacman-key --add -
-pacman-key --lsign-key DD90A9CDD96977DDD9CEA9491C4AEDE69936E27C
-
-echo "[*] Refreshing pacman keys..."
-pacman-key --refresh-keys
-
 echo "=== Customization complete ==="
--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 mkdir -p {out,work}
-curl -sSL https://asiraos.github.io/core/asiraos-core.pubkey.asc | sudo pacman-key --add -
 sudo mkarchiso -v -w work -o out .
